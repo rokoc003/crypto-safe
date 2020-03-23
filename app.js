98ac82339f427f821583a215e92a2d57697eb8f6
rokoc003 <roxane.kocem@yahoo.de>
2020-03-23 17:09:26 +0100

    Add get, set and unset functions and refactor in a seperate file

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-function sayHi() {
-  console.log("Click");
+const { readPasswords, writePasswords } = require("./lib/passwords");
+
+const [command, key, value] = process.argv.slice(2);
+
+function get() {
+  console.log("Called GET", key);
+  try {
+    const passwords = readPasswords();
+    console.log(passwords[key]);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-sayHi();
+function set() {
+  console.log("Called SET", key, value);
+  try {
+    const passwords = readPasswords();
+    passwords[key] = value;
+    writePasswords();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function unset() {
+  console.log("Called UNSET", key);
+  try {
+    const passwords = readPasswords();
+    delete passwords[key];
+    writePasswords();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+if (command === "get") {
+  get();
+} else if (command === "set") {
+  set();
+} else if (command === "unset") {
+  unset();
+} else {
+  console.error("Unknown command");
+}
