814621d76c2178ae5faa51cfa940a2ae2e6eb136
rokoc003 <roxane.kocem@yahoo.de>
2020-03-24 11:17:29 +0100

    Ask user for password

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const [command, key, value] = process.argv.slice(2);
 const { get, set, unset } = require("./lib/commands");
+const { askForPassword } = require("./lib/questions");
 
-if (command === "get") {
-  get(key);
-} else if (command === "set") {
-  set(key, value);
-} else if (command === "unset") {
-  unset(key);
-} else {
-  console.error("Unknown command");
+const [command, key] = process.argv.slice(2);
+
+async function run() {
+  if (command === "get") {
+    get(key);
+  } else if (command === "set") {
+    const password = await askForPassword(key);
+    set(key, password);
+  } else if (command === "unset") {
+    unset(key);
+  } else {
+    console.error("Unknown command");
+  }
 }
+run();
