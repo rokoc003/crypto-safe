a61923d3c9d0e7198bed5407d61bdc932e141593
rokoc003 <roxane.kocem@yahoo.de>
2020-03-25 16:34:05 +0100

    Connection to MongoDB

--- app.js
+++ app.js
@@ -2,29 +2,38 @@ const { get, set, unset, reset } = require("./lib/commands");
 const { askForPassword, askForMasterPassword } = require("./lib/questions");
 const { readMasterPassword } = require("./lib/passwords");
 const { verifyHash } = require("./lib/crypto");
+const { connect, close } = require("./lib/db");
 
 const [command, key] = process.argv.slice(2);
 
 async function run() {
-  const answeredMasterPassword = await askForMasterPassword();
-  if (command === "reset") {
-    return reset(answeredMasterPassword);
-  }
-  const masterPassword = readMasterPassword();
-  if (!verifyHash(answeredMasterPassword, masterPassword)) {
-    console.error("You have no access!");
-    return;
-  }
+  try {
+    await connect();
+    const answeredMasterPassword = await askForMasterPassword();
+    if (command === "reset") {
+      return reset(answeredMasterPassword);
+    }
+    const masterPassword = readMasterPassword();
 
-  if (command === "get") {
-    get(key);
-  } else if (command === "set") {
-    const password = await askForPassword(key);
-    set(key, password);
-  } else if (command === "unset") {
-    unset(key);
-  } else {
-    console.error("Unknown command");
+    if (!verifyHash(answeredMasterPassword, masterPassword)) {
+      console.error("You have no access!");
+      return;
+    }
+
+    if (command === "get") {
+      get(key);
+    } else if (command === "set") {
+      const password = await askForPassword(key);
+      set(key, password);
+    } else if (command === "unset") {
+      unset(key);
+    } else {
+      console.error("Unknown command");
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await close();
   }
 }
 run();
